742d60359810e05296cb03b24de643497180a6d3
Jonathan "JonLem" Lemos <38771842+JonLemOfficial@users.noreply.github.com>
2020-06-20 13:20:17 -0400

    Core files and dirs for web app

---
+++ src/app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = process.argv[2] || 8080;
+const app = express();
+
+var routes = require('./routes');
+
+app
+  .use(express.static(path.join(__dirname, "../public")))
+  .set('view engine', "ejs")
+
+  .get('/', routes.home)
+  .get('/login', routes.login)
+
+  // init server
+  .listen(PORT, () => {
+    console.log(`Server running at: http://localhost:${PORT}/`)
+  });
