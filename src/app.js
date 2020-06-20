32d8acb740e32e3a318509f7e039cf6cbc214014
Jonathan "JonLem" Lemos <38771842+JonLemOfficial@users.noreply.github.com>
2020-06-20 14:04:46 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const PORT = process.argv[2] || 8080;
+const PORT = process.env.PORT || process.argv[2] || 8080;
 const app = express();
 
 var routes = require('./routes');
